cb650c9e7f5bdf87d1853b57b663e2613f43ae5f
Shawn Ren <shawnren527@gmail.com>
2019-05-14 02:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const result = []
 function writeCards(names, eventName){
-  for (let name of name_arr) {
+  for (let name of names) {
     return result.push(`Thank you, ${name}, for the wonderful ${occasion} gift!` )
   }
 }
