7e9d5d97bc311b5ce5466c06f21c7306853ff2f5
Shawn Ren <shawnren527@gmail.com>
2019-05-14 03:10:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const result = []
+function writeCards(names, eventName){
+  for (let name of names) {
+    result.push(`Thank you, ${name}, for the wonderful ${enventName} gift.` )
+  }
+  return result
+}
+
+function countdown(num) {
+  let countdown = num
+  while (countdown >= 0) {
+    console.log(countdown--)
+  }
+}
